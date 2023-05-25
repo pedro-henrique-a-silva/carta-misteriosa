ee402343d4da37130bea156474dff990ffd19b8c
Pedro Henrique A. Silva <pedro.henrique.a.s66@gmail.com>
2023-05-25 20:54:55 -0300

    requisito 17 concluido, mudando fundo das palavras

--- script.js
+++ script.js
@@ -1,25 +1,35 @@
 const form = document.querySelector('#section-carta form');
 const pcarta = document.querySelector('#carta-gerada');
+
 const fundos = ['newspaper', 'magazine1', 'magazine2'];
 const tamanho = ['medium', 'big', 'reallybig'];
-const efeitos = ['rotateleft', 'skewleft', 'rotateright', 'skewright'];
+const efeitos1 = ['rotateleft', 'rotateright'];
+const efeitos2 = ['skewleft', 'skewright'];
 
 const generateRandomNumber = (number) => Math.floor(Math.random() * (number));
+
+const generateRandomStyle = () => {
+  const fundoEscolhido = fundos[generateRandomNumber(fundos.length)];
+  const efeitoRotacao = efeitos1[generateRandomNumber(efeitos1.length)];
+  const efeitoInclinacao = efeitos2[generateRandomNumber(efeitos2.length)];
+  const tamanhoEscolhido = tamanho[generateRandomNumber(tamanho.length)];
+
+  return [fundoEscolhido, efeitoRotacao, efeitoInclinacao, tamanhoEscolhido];
+};
 
 const geraCartaMisteriosa = (frase) => {
   const arrayPalavras = frase.split(' ');
   pcarta.innerHTML = '';
   for (let index = 0; index < arrayPalavras.length; index += 1) {
-    const fundoEscolhido = fundos[generateRandomNumber(fundos.length)];
-    const efeitoEscolhido = efeitos[generateRandomNumber(efeitos.length)];
-    const tamanhoEscolhido = efeitos[generateRandomNumber(tamanho.length)];
+    const estilos = generateRandomStyle();
 
     const span = document.createElement('span');
 
     span.innerText = arrayPalavras[index];
-    span.classList.add(fundoEscolhido);
-    span.classList.add(efeitoEscolhido);
-    span.classList.add(tamanhoEscolhido);
+    span.classList.add(estilos[0]);
+    span.classList.add(estilos[1]);
+    span.classList.add(estilos[2]);
+    span.classList.add(estilos[3]);
 
     pcarta.appendChild(span);
   }
@@ -37,4 +47,21 @@ const formEvent = () => {
   });
 };
 
-formEvent(); 
+const palavraEvent = () => {
+  pcarta.addEventListener('click', (event) => {
+    if (event.target.tagName === 'SPAN') {
+      const estilos = generateRandomStyle();
+      const elemento = event.target;
+      elemento.className = '';
+      console.log(estilos);
+
+      elemento.classList.add(estilos[0]);
+      elemento.classList.add(estilos[1]);
+      elemento.classList.add(estilos[2]);
+      elemento.classList.add(estilos[3]);
+    }
+  });
+};
+
+formEvent();
+palavraEvent();
