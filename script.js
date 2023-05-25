cc3a3171b464a3cf034923155d4fe64b7f11e821
Pedro Henrique A. Silva <pedro.henrique.a.s66@gmail.com>
2023-05-25 20:21:04 -0300

    Resquisitos ate o 16 concluidos

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const form = document.querySelector('#section-carta form');
 const pcarta = document.querySelector('#carta-gerada');
-const fundos = ['misterio1', 'misterio2', 'misterio3'];
+const fundos = ['newspaper', 'magazine1', 'magazine2'];
+const tamanho = ['medium', 'big', 'reallybig'];
 const efeitos = ['rotateleft', 'skewleft', 'rotateright', 'skewright'];
 
 const generateRandomNumber = (number) => Math.floor(Math.random() * (number));
@@ -11,12 +12,14 @@ const geraCartaMisteriosa = (frase) => {
   for (let index = 0; index < arrayPalavras.length; index += 1) {
     const fundoEscolhido = fundos[generateRandomNumber(fundos.length)];
     const efeitoEscolhido = efeitos[generateRandomNumber(efeitos.length)];
+    const tamanhoEscolhido = efeitos[generateRandomNumber(tamanho.length)];
 
     const span = document.createElement('span');
 
     span.innerText = arrayPalavras[index];
     span.classList.add(fundoEscolhido);
     span.classList.add(efeitoEscolhido);
+    span.classList.add(tamanhoEscolhido);
 
     pcarta.appendChild(span);
   }
@@ -26,8 +29,12 @@ const formEvent = () => {
   form.addEventListener('submit', (event) => {
     event.preventDefault();
     const fraseInput = document.querySelector('#carta-texto');
-    geraCartaMisteriosa(fraseInput.value);
+    if (fraseInput.value.trim()) {
+      geraCartaMisteriosa(fraseInput.value);
+    } else {
+      pcarta.innerText = 'Por favor, digite o conteúdo da carta.';
+    }
   });
 };
 
-formEvent();
+formEvent(); 
